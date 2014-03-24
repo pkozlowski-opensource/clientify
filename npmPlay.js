ae16be458f72362a04793bf7ee043bf83acae6b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-03-24 22:18:26 +0100

    feat: make it work with async npm registry

--- npmPlay.js
+++ npmPlay.js
@@ -1,13 +1,29 @@
 var npm = require('npm');
 var q = require('q');
+var npmFlatten = require("./lib/npmFlatten.js");
 var npmLoad = q.nbind(npm.load, npm);
 
-npmLoad().then(function(npm){
+npmLoad().then(function (npm) {
+
+  var cache = {};
 
   var registryGet = q.nbind(npm.registry.get, npm.registry);
+  var registry = {
+    get: function (pkgName) {
+      if (cache[pkgName]) {
+        return q.when(cache[pkgName])
+      } else {
+        return registryGet(pkgName).then(function (data) {
+          cache[pkgName] = data[0];
+          return data[0];
+        })
+      }
+    }
+  };
 
-  registryGet('stowe.js').then(function(data) {
-    console.log(data);
-  });
-
+  npmFlatten.resolvePackages(registry, {
+    'di': '*'
+  }).then(function (result) {
+    console.log(result);
+  }).done();
 });
