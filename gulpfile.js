5f3e9c4d4e6e1ddeaf3312b0bee1e30b6a8d40dd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-03-24 20:47:05 +0100

    chore: cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
-  gulp.src('test/algorithm.spec.js', { read: false })
+  gulp.src(['lib/**/.js', 'test/**/*.spec.js'], { read: false })
     .pipe(mocha())
     .on('error', gutil.log);
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['lib/*.js', 'test/*.spec.js'], ['default']);
+  gulp.watch(['lib/**/.js', 'test/**/*.spec.js'], ['default']);
 });
